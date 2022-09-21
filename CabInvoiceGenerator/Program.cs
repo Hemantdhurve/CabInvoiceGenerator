cfdeebba2127c49ac67791eb611c61ba1b7a5b26
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-21 23:42:13 +0530

    Welcome Message

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Problem");
+           
+        }
+    }
+}
