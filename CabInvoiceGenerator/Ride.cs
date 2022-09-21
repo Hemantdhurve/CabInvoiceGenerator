2ad0ca20fd2e3e9ebf49d0f1980068cc5562bf10
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-21 23:45:51 +0530

    [Hemant] Added Cab Invoice Generator

---
+++ CabInvoiceGenerator/Ride.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    /// <summary>
+    /// Ride Class to set Data for Particular Ride
+    /// </summary>
+    public class Ride
+    {
+        public double distance;
+        public int time;
+        /// <summary>
+        /// Parameter Constructor for Setting data
+        /// </summary>
+        
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
---
+++ CabInvoiceGenerator/RideType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    /// <summary>
+    /// Enum class for Cab Invoice
+    /// </summary>
+    public enum RideType
+    {
+        NORMAL, 
+        PREMIUM
+    }
+}
